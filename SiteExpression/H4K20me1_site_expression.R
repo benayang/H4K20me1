8360d5aec5d53361eb0a832776771388fd956791
Benjamin Yang <32141359+benayang@users.noreply.github.com>
2020-07-12 19:26:21 -0400

    Add files via upload

--- SiteExpression/H4K20me1_site_expression.R
+++ SiteExpression/H4K20me1_site_expression.R
@@ -16,9 +16,9 @@ unique.young.genes.expr$logFC = vals$logFC[vals$gene %in% unique.young.genes.exp
 unique.aged.genes.expr$FDR = vals$adj.P.Val[vals$gene %in% unique.aged.genes.expr$gene]
 unique.young.genes.expr$FDR = vals$adj.P.Val[vals$gene %in% unique.young.genes.expr$gene]
 
-write.table(unique.young.genes.expr, file="/nas/homes/benyang/H4K20me1/Figures/H4K20me1_site_expression/unique_young_genes_expr.txt",
+write.table(unique.young.genes.expr[order(unique.young.genes.expr$FDR),], file="/nas/homes/benyang/H4K20me1/Figures/H4K20me1_site_expression/unique_young_genes_expr.txt",
             sep="\t", quote=F, row.names = F)
-write.table(unique.aged.genes.expr, file="/nas/homes/benyang/H4K20me1/Figures/H4K20me1_site_expression/unique_aged_genes_expr.txt",
+write.table(unique.aged.genes.expr[order(unique.aged.genes.expr$FDR),], file="/nas/homes/benyang/H4K20me1/Figures/H4K20me1_site_expression/unique_aged_genes_expr.txt",
             sep="\t", quote=F, row.names = F)
 
 write.table(subset(unique.young.genes.expr, subset=abs(logFC)>1 & FDR<0.05), 
