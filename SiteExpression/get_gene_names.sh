e54ce7a1e7679cb5c8dac1f90c197e1d2950e7e9
Benjamin Yang <32141359+benayang@users.noreply.github.com>
2020-07-13 01:30:05 -0400

    Add files via upload

--- SiteExpression/get_gene_names.sh
+++ SiteExpression/get_gene_names.sh
@@ -2,9 +2,9 @@
 
 pjdir="/nas/homes/benyang/H4K20me1/Figures/H4K20me1_site_expression"
 
-cut -f 4 $pjdir/Aged.nearest.tss.bed | sort | uniq > $pjdir/Aged.genenames.txt
+cut -f 4 $pjdir/Aged.5kb.tss.bed | sort | uniq > $pjdir/Aged.genenames.txt
 
-cut -f 4 $pjdir/Young.nearest.tss.bed | sort | uniq > $pjdir/Young.genenames.txt
+cut -f 4 $pjdir/Young.5kb.tss.bed | sort | uniq > $pjdir/Young.genenames.txt
 
 grep -w -f $pjdir/Aged.genenames.txt $pjdir/Young.genenames.txt > $pjdir/common.genenames.txt
 grep -v -w -f $pjdir/Aged.genenames.txt $pjdir/Young.genenames.txt > $pjdir/unique.Young.genenames.txt 
